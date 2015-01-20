43d572683977d6734660efdc576c0348c1175698
errnio <errnio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 17:43:49 +0000

    1. adding async attr to loader script 2. injecting script in footer 3. options page styles update 4. fixing contact-us link
    git-svn-id: https://plugins.svn.wordpress.org/errnio-mobile-gesture-monetization/trunk@1071837 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- errnio.php
+++ errnio.php
@@ -25,16 +25,16 @@ function errnio_options() {
 
 	wp_register_style( 'errnio-style', plugins_url( 'css/style.css', __FILE__ ) );
 	wp_enqueue_style( 'errnio-style' );
-	
+
 ?>
 	<form method="post" action="options.php">
 		<div class="wrap">
 			<h2>Errnio Options</h2>
 			<div class="wrap-options">
-				
-				
+
+
 					<?php wp_nonce_field('update-options'); ?>
-					
+
 					<div class="wrap-options-left">
 						<div class="element">
 							<label>Errnio code:</label>
@@ -43,26 +43,27 @@ function errnio_options() {
 					</div>
 					</br>
 					<div class="wrap-options-separator"></div>
-					
+
 					<div class="wrap-options-right">
 						<div class="element">
-							Don't have an errnio site ID? <a class="publisher_button" href="http://errnio.com/#how-to-contact-errnio">Become a Publisher</a> </div></br>
+							Don't have an errnio site ID? <a class="publisher_button" href="http://errnio.com/#contact" target="_blank">Become a Publisher</a> </div></br>
 							<div class="description_small">Grab our unique code for your site and go live today.</div>
+                            <div class="description_small" style="font-weight:bold;"> Here's a quick, easy step by step installation <a href="http://errnio.com/WP/wordpress-instructions.pdf" target="_blank">guide</a></div>
 						</div>
-					</div>	
-						
+					</div>
+
 					<div class="clear"></div>
-					
+
 					<input type="hidden" name="page_options" value="errnio_api" />
 					<input type="hidden" name="action" value="update" />
-					
-				
+
+
 			</div>
 		</div>
-		
+
 		<input class="button button-primary wrap-options-submit" type="submit" value="Save Changes" />
-		
-	</form>	
+
+	</form>
 <?php
 }
 
@@ -71,17 +72,29 @@ function errnio_load_script()
 {
 	if(get_option('errnio_api')){
 		$script_url = "//service.errnio.com/loader?tagid=".get_option('errnio_api');
-		
-		wp_register_script( 'errnio_script', $script_url, array( 'jquery' ), '1.0', false );
+
+		wp_register_script( 'errnio_script', $script_url, array( 'jquery' ), '1.0', true );
 		wp_enqueue_script( 'errnio_script');
 	}
 	else{
 		$script_url = "//service.errnio.com/loader?tagid=54982f10154585edd75615e0";
-		
-		wp_register_script( 'errnio_script', $script_url, array( 'jquery' ), '1.0', false );
+
+		wp_register_script( 'errnio_script', $script_url, array( 'jquery' ), '1.0', true );
 		wp_enqueue_script( 'errnio_script');
 	}
 }
+
+function add_async_attr( $url )
+{
+	if(FALSE === strpos( $url, 'service.errnio.com')){
+		return $url;
+	}
+
+	return "$url' async='async";
+}
+
+add_filter( 'clean_url', 'add_async_attr', 11, 1);
+
 add_action( 'wp_enqueue_scripts', 'errnio_load_script', 99999 );
 
 
@@ -89,6 +102,6 @@ add_action( 'wp_enqueue_scripts', 'errnio_load_script', 99999 );
 add_action( 'admin_notices', 'wptuts_admin_notices' );
 function wptuts_admin_notices() {
 	if(!get_option('errnio_api')){
-		echo( '<div class="error"> <p> Errnio needs to be configured, enter your code <a href="'.admin_url( 'options-general.php?page=errnio.php' ).'">here</a> </p> </div>');
+		echo( '<div class="error" style="font-weight:bold;font-size=22px;color:red;"> <p> Errnio needs to be configured, enter your code <a href="'.admin_url( 'options-general.php?page=errnio.php' ).'">here</a> </p> </div>');
 	}
 }
